3238fdd88b7c7d0ecb0bf593c5231cfc1b5b0bad
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2026-08-20 22:42:28 -0300

    Libs

---
+++ TP1/E1/cliente.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <sys/types.h>
+
+
+//Linux 
+
+//#include <sys/socket.h>  
+//#include <netinet/in.h> 
+
+
+//Windows
+
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
